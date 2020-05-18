0c609c26991810645a2467e8f4e0b62c53e30929
Inés Prieto González <ipgonzalez2@esei.uvigo.es>
2020-05-18 22:10:56 +0200

    testing

--- parse-mail.py
+++ parse-mail.py
@@ -136,7 +136,7 @@ def filter():
 
 def notifier():
   notifier = pyinotify.AsyncNotifier(wm, EventHandler())
-  wdd = wm.add_watch('/home/inesp', mask, rec=False)
+  wdd = wm.add_watch('/spam', mask, rec=False)
   import asyncore
   asyncore.loop()
 
