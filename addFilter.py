01aa5d14aea4744824c5ad946e9bbaf116c77bca
Inés Prieto González <ipgonzalez2@esei.uvigo.es>
2020-05-19 11:49:43 +0200

    testing addFilter

--- addFilter.py
+++ addFilter.py
@@ -8,56 +8,25 @@ from jinja2 import Environment, FileSystemLoader
 from sys import argv
 from os import path
 
-#args
-def usage():
-    print("USAGE: %s [-p <percentage_characters>(4,5,6,7)] spam_file" % argv[0])
-    print("")
-    print("Try '%s -h' for more options." % argv[0])
-    exit()
 
-#help
-def help():
-    print("USAGE: %s [-p <percentage_characters>(4,5,6,7)] spam_file" % argv[0])
-    print("")
-    print("optional arguments:")
-    print("   -h                       print this help")
-    print("   -p percentage_characters               Checks certain number of characters. Default is 4%")
-    print("")
-    print("examples:")
-    print("    addFilter spam.eml             # adds filter to spam.eml checking 4% of the email")
-    print("    addFilter -p 5 spam.eml        # adds filter to spam.eml checking 5% of the email")
-    exit()
+BLOCK_SIZE = 65536
 
-#arguments
-porcentaje = 4
+def getHash(file_path):
+  file_hash = hashlib.sha256()
+  with open(file_path, 'rb') as f:
+    fb = f.read(BLOCK_SIZE)
+    while len(fb) > 0:
+        file_hash.update(fb)
+        fb = f.read(BLOCK_SIZE)
+  return file_hash
+        
 
-if len(argv) == 2:
-  if str(argv[1]) == '-h':
-    help()
-  elif path.exists(argv[1]):
-    file_path = argv[1]  
-  else:
-    usage()
-
-if len(argv) == 3:
-  if str(argv[1]) == '-p':
-    help()
-  else:
-    usage()
-
-if len(argv) == 4:
-  if str(argv[1]) == '-p' and path.exists(argv[3]):
-    porcentaje = int(argv[2])
-    file_path = argv[3]
-  else:
-    usage()
-
-if len(argv) > 4 or len(argv) < 2:
-  usage()
-
+# Read configuration
 config = ConfigParser.RawConfigParser()
 config.read('filters.cfg')
 porcentaje = config.get('settings','percentage')
+if path.exists(argv[1]):
+  file_path = argv[1]
 
 # Open given spam to be filtered 
 fileSpam = open(file_path, 'r')
@@ -92,6 +61,8 @@ else:
 
 fileSpam = open(file_path, 'r')
 
+
+# Getting characters to compare
 for i in range(numCar):
     desp = inicioMensaje + (x * i)
     caracter = fileSpam.read()[desp]
@@ -103,7 +74,7 @@ for i in range(numCar):
 
 fileSpam.close()
 
-# Updating the configuration file
+# Searching filter id
 config = ConfigParser.RawConfigParser()
 config.read("filters.cfg")
 if(len(config.sections()) > 1):
@@ -125,13 +96,7 @@ with open("./filters/filter"+numFilter+".c", "w") as fh:
     fh.write(output)
 
 # Calculating hash of file
-BLOCK_SIZE = 65536
-file_hash = hashlib.sha256()
-with open(file_path, 'rb') as f:
-    fb = f.read(BLOCK_SIZE)
-    while len(fb) > 0:
-        file_hash.update(fb)
-        fb = f.read(BLOCK_SIZE)
+file_hash = getHash(file_path)
 
 # Adding section to configuration file
 config.add_section('Filter'+numFilter)
