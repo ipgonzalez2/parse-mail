d495071e3cfde642f861757fd546c69fccbf0eb7
Inés Prieto González <ipgonzalez2@esei.uvigo.es>
2020-05-19 13:46:00 +0200

    testing

--- parse-mail.py
+++ parse-mail.py
@@ -69,24 +69,19 @@ def filter():
   for section in config.sections()[1:]:
     hashes.append(config.get(section, 'hash'))
 
-  print("HASHES INICIALES: " + str(hashes))
-
 
   # Adding filter for files in spam/ if needed
   for entry in os.listdir(basepath):
     if os.path.isfile(os.path.join(basepath, entry)) and entry != '.gitkeep' :
       hash_summary = utils.getHash(os.path.join(basepath, entry)).hexdigest()
-      print(hash_summary)
       if hash_summary not in hashes:
         print("adding filter for" + str(entry))
         utils.addFilter(os.path.join(basepath, entry))
       else:
-        print("removing filter for" + str(entry))
         hashes.remove(hash_summary)
 
-  print("hashes despues " + str(hashes))
 
-
+  # Removing filters if not in directory spam/
   config.read('filters.cfg')
   for section in config.sections()[1:]:
     if config.get(section, 'hash') in hashes:
@@ -101,35 +96,35 @@ def filter():
 
   config.read('filters.cfg')
 
+
+  # Creating sockets
   for filter in config.sections()[1:]:
     program = config.get(filter,'program')
     function = config.get(filter,'function')
 
-  # initialize BPF - load source code from http-parse-simple.c
+    # initialize BPF - load source code from filters/program.c
     bpf.append(BPF(src_file = "filters/"+program,debug = 0))
-    #bpf = BPF(src_file = "filters/"+program,debug = 0)
 
-  #load eBPF program http_filter of type SOCKET_FILTER into the kernel eBPF vm
-  #more info about eBPF program types
-  #http://man7.org/linux/man-pages/man2/bpf.2.html
+    #load eBPF program function of type SOCKET_FILTER into the kernel eBPF vm
     #function_http_filter = bpf.load_func(function, BPF.SOCKET_FILTER)
     function_http_filter.append(bpf[-1].load_func(function, BPF.SOCKET_FILTER))
 
-  #create raw socket, bind it to interface
-  #attach bpf program to socket created
+    #create raw socket, bind it to interface
+    #attach bpf program to socket created
     BPF.attach_raw_socket(function_http_filter[-1], interface)
 
-  #get file descriptor of the socket previously created inside BPF.attach_raw_socket
-    #socket_fd = function_http_filter.sock
+    #get file descriptor of the socket previously created inside BPF.attach_raw_socket
     socket_fd.append(function_http_filter[-1].sock)
     config.set(filter, 'fd', function_http_filter[-1].sock)
 
-  #create python socket object, from the file descriptor
-    #sock = socket.fromfd(socket_fd,socket.PF_PACKET,socket.SOCK_RAW,socket.IPPROTO_IP)
+    #create python socket object, from the file descriptor
     sock.append(socket.fromfd(socket_fd[-1],socket.PF_PACKET,socket.SOCK_RAW,socket.IPPROTO_IP))
-  #set it as blocking socket
+
+    #set it as blocking socket
     sock[-1].setblocking(True)
-    print(socket_fd)
+
+    with open('filters.cfg', 'wb') as configfile:
+      config.write(configfile)
 
   while 1:
     for i in socket_fd:
