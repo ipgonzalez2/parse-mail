4194e9bec6711b924ca70e241d8894cb8bcf1741
Inés Prieto González <ipgonzalez2@esei.uvigo.es>
2020-05-20 14:35:29 +0200

    testing

--- utils_test.py
+++ utils_test.py
@@ -14,23 +14,16 @@ class TestUtils(unittest.TestCase):
             self.config.write(configfile)
 
 
-    # Test for spams with array of size < 30
-    def test_add_1(self):
-        numCaracteres, caracteres = utils.addFilter('test/file_test1', 'test/filters_test_add.cfg')
-        self.assertEqual(numCaracteres, 14)
-        self.assertEqual(caracteres, ['-','_','t','e','r','\n','g','I','C','W','2','J','u','B'])
-        self.config.read('test/filters_test_add.cfg')
-        self.assertTrue(len(self.config.sections()) == 2)
-
-
-    # Test for spams with size > 15000
-    def test_add_2(self):
-        numCaracteres, caracteres = utils.addFilter('test/file_test2', 'test/filters_test_add.cfg')
-        self.assertEqual(numCaracteres, 30)
-        self.assertEqual(caracteres, [' ','n','\xfa',' ',' ',' ',' ','b','/','V','x','9','H','v','j','f','w','E','3','a','p','2','M','t','p','g','O','d','7','y'])
+    # Test for spams with array of size < 30 and size > 15000
+    def test_add(self):
+        numCaracteres1, caracteres1 = utils.addFilter('test/file_test1', 'test/filters_test_add.cfg')
+        self.assertEqual(numCaracteres1, 14)
+        self.assertEqual(caracteres1, ['-','_','t','e','r','\n','g','I','C','W','2','J','u','B'])
+        numCaracteres2, caracteres2 = utils.addFilter('test/file_test2', 'test/filters_test_add.cfg')
+        self.assertEqual(numCaracteres2, 30)
+        self.assertEqual(caracteres2, [' ','n','\xfa',' ',' ',' ',' ','b','/','V','x','9','H','v','j','f','w','E','3','a','p','2','M','t','p','g','O','d','7','y'])
         self.config.read('test/filters_test_add.cfg')
         self.assertTrue(len(self.config.sections()) == 3)
-
 
 
     def tearDown(self):
