b8faab34af859ecdff45bafd5f59ad3f8781ad0a
Inés Prieto González <ipgonzalez2@esei.uvigo.es>
2020-06-08 13:50:03 +0200

    testing

--- prueba.py
+++ prueba.py
@@ -79,6 +79,7 @@ def filter():
     #set it as blocking socket
   sock.setblocking(True)
   bpf.disassemble_func(function)
+  bpf.cleanup()
   print(function_http_filter.bpf)
   print(dir(function_http_filter.sock))
 
