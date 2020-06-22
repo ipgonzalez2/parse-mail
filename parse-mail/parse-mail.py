eaa4873a343dc080ffd6048e3e748d5eaf9f5fd6
Inés Prieto González <ipgonzalez2@esei.uvigo.es>
2020-06-22 14:03:30 +0200

    testing

--- parse-mail/parse-mail.py
+++ parse-mail/parse-mail.py
@@ -222,7 +222,7 @@ def filter():
   while 1:
     for fd in socket_fd:
       f = open("results.txt", "a")
-      f.write(str(socket_fd))
+      f.write(socket_fd)
       f.write(os.read(fd, 10000))
       f.close()
 
